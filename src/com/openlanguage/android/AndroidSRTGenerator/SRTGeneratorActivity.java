f217a667e3f6d56412a45a51f064ced7804cf34c
sid <siddharthakattoju@gmail.com>
2011-06-04 13:40:08 -0400

    initial commit for SRTgenerator library

---
+++ src/com/openlanguage/android/AndroidSRTGenerator/SRTGeneratorActivity.java
@@ -0,0 +1,13 @@
+package com.openlanguage.android.AndroidSRTGenerator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SRTGeneratorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
