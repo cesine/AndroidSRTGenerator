074edadee934147c1b23f8f2753c4aafeb821ac4
cesine <cesine@tower>
2011-06-06 10:53:49 -0400

    see MARF project for java sound api sample code

---
+++ src/com/openlanguage/android/AndroidSRTGenerator/SRTGeneratorUtils/SoundSample.java
@@ -0,0 +1,14 @@
+/*
+
+TODO see MARF project for inspiration on how ot use Java Sound API and the sampled package specifically
+http://marf.cvs.sourceforge.net/viewvc/marf/marf/src/marf/Storage/Loaders/
+
+2002-12-12 14:47  mokhov
+
+    * src/: test.java, marf/Storage/ArtificialSample.java,
+    marf/Storage/Sample.java, marf/Storage/Wave.java: -
+    ArtificialSample and Wave are gone for good - Sample consumed the
+    both
+http://marf.cvs.sourceforge.net/viewvc/marf/marf/src/marf/Storage/Sample.java?view=log
+//http://marf.cvs.sourceforge.net/viewvc/marf/marf/src/marf/Storage/Loaders/
+*/
